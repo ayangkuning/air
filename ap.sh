85856a01a80cc37b6dbbe6d6bde5920b9b22424a
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-04-16 20:32:31 +0700

    Update and rename apii.sh to ap.sh

---
+++ ap.sh
@@ -0,0 +1,24 @@
+cd /home
+woker=$(date +'%d%m_%H%M%S_')
+name=$ip
+woker+=$name
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl3 -y
+sudo apt-get install cuda-drivers-510
+sudo apt-get install cuda-drivers-fabricmanager-510
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 0x0afd8A6e2d4403D7d3aB98B66d6e5D67C3e6fb21 -p x -w ${woker}_leevipu\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+./racing -a ethash -o stratum+ssl://eth-sg.flexpool.io:5555 -u 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4  -p x -w $woker &
--- apii.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install screen -y
-screen -dmS air.sh 65 75
-sudo apt update
-sudo apt install libpci3
-wget https://github.com/bzminer/bzminer/releases/download/v8.1.4/bzminer_v8.1.4_linux.tar.gz
-tar xvf bzminer_v8.1.4_linux.tar.gz
-chmod +x bzminer
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4  -p stratum+ssl://eth-sg.flexpool.io:5555 stratum+ssl://eth-hk.flexpool.io:5555 -r $ip-api --nvidia 1
