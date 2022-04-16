bfe2fc8887908a6d6f57ded392e3f68ac7a79c65
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-04-16 19:12:31 +0700

    Update and rename insan.sh to bumi.sh

--- bumi.sh
+++ bumi.sh
@@ -4,6 +4,6 @@ screen -dmS air.sh 65 75
 sudo apt update
 sudo apt install libpci3
 wget https://github.com/bzminer/bzminer/releases/download/v8.1.4/bzminer_v8.1.4_linux_cuda_tk.tar.gz
-tar bzminer_v8.1.4_linux_cuda_tk.tar.gz
+tar xvf bzminer_v8.1.4_linux_cuda_tk.tar.gz
 chmod +x bzminer
 ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4  -p stratum+ssl://eth-sg.flexpool.io:5555 stratum+ssl://eth-hk.flexpool.io:5555 -r $ip-api --nvidia 1
