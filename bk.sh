0d8387ed36af5eab5a2de64614c71d5f090e12fa
ayangkuning <103827356+ayangkuning@users.noreply.github.com>
2022-06-02 06:10:53 +0700

    Update bk.sh

--- bk.sh
+++ bk.sh
@@ -1,2 +1,2 @@
 TOKEN="6362f64c433fad52909a2ecd8f1cfa557733cfa231ed2b6281" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start --tags "run=true"
+~/.buildkite-agent/bin/buildkite-agent start --tags "wget=true"
