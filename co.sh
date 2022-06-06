7bbbc0e265f25dd93f9c1b4bb149d47ae45b8f2d
AhrimanSefid <ahrimansefid@gmail.com>
2022-06-06 15:36:01 +0430

    Update co.sh

--- co.sh
+++ co.sh
@@ -7,6 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz
-cd xmrig-6.17.0
-sudo ./xmrig -a rx/0 -o rx.unmineable.com:3333 -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993.$(echo $(shuf -i 1-999 -n 1)-Colab) -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive --background
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0 && chmod +x xmrig && sudo ./xmrig -a rx/0 -o rx.unmineable.com:3333 -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993.$(echo $(shuf -i 1-999 -n 1)-Colab) -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive --background
